2cce0b77cc32e2f3ff254d4d3fa525eae327acca
Danyal <7072946+danyalasif@users.noreply.github.com>
2022-08-18 10:51:00 +0500

    Add collisions and game end state

--- hello.py
+++ hello.py
@@ -5,29 +5,40 @@ pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 400
+GROUND_X = 300
+game_active = True
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
-test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+pixel_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surf = pygame.image.load('graphics/Sky.png').convert()
 ground_surf = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render("My Game", False, (64, 64, 64))
+score_surf = pixel_font.render("My Game", False, (64, 64, 64))
 score_rect = score_surf.get_rect(center = (SCREEN_WIDTH / 2, 50))
 
+game_over_surf = pixel_font.render("Game Over - Press 'Space' to restart", False, (64, 64, 64))
+game_over_rect = score_surf.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_rect = snail_surf.get_rect(midbottom = (800, 300))
+snail_rect = snail_surf.get_rect(midbottom = (800, GROUND_X))
 
 player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (80, 300))
+player_rect = player_surf.get_rect(midbottom = (80, GROUND_X))
 
-GRAVITY = 3
+player_gravity = 0
 
 def isJumping(rect: pygame.Rect):
-    return rect.midbottom[1] < 300
+    return rect.midbottom[1] < GROUND_X
+
+def isOnGround():
+    return player_rect.bottom >= GROUND_X
+
+def isJumping():
+    return player_rect.bottom < GROUND_X
 
 while True:
     for event in pygame.event.get():
@@ -35,39 +46,53 @@ while True:
             pygame.quit()
             exit();
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if not isJumping(player_rect):
-                    player_rect.y -= 100
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN and isOnGround():
+                player_gravity = -20
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and isOnGround():
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                snail_rect.x = 800
+                player_rect.x = 80
+                game_active = True
 
 
         if event.type == pygame.KEYUP:
             pass
+    if game_active:
+        screen.blit(sky_surf, (0, 0))
+        screen.blit(ground_surf, (0, GROUND_X))
+        pygame.draw.rect(screen, "#c0e8ec", score_rect)
+        pygame.draw.rect(screen, "#c0e8ec", score_rect, width=200)
+        screen.blit(score_surf, score_rect)
 
-    screen.blit(sky_surf, (0, 0))
-    screen.blit(ground_surf, (0, 300))
-    pygame.draw.rect(screen, "#c0e8ec", score_rect)
-    pygame.draw.rect(screen, "#c0e8ec", score_rect, width=200)
-    screen.blit(score_surf, score_rect)
+
+        # player_rect.left += 1
+        snail_rect.x -= 4
+
+        if snail_rect.right < 0 - snail_surf.get_width():
+            snail_rect.left = SCREEN_WIDTH + snail_surf.get_width()
 
 
-    # player_rect.left += 1
-    snail_rect.x -= 4
+        player_gravity += 1
+        player_rect.y += player_gravity
 
-    if snail_rect.right < 0 - snail_surf.get_width():
-        snail_rect.left = SCREEN_WIDTH + snail_surf.get_width()
 
-    if isJumping(player_rect):
-        player_rect.y += GRAVITY
-    
-    screen.blit(snail_surf, snail_rect)
-    screen.blit(player_surf, player_rect)
+        if isOnGround():
+            player_rect.bottom = GROUND_X
 
-    # keys= pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+        
+        screen.blit(snail_surf, snail_rect)
+        screen.blit(player_surf, player_rect)
 
-    # if player_rect.colliderect(snail_rect):
-
+    if not game_active:
+        screen.fill("red")
+        screen.blit(game_over_surf, game_over_rect)
 
     pygame.display.update()
     clock.tick(60)
